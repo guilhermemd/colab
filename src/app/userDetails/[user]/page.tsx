220dbcd30479e7a21b1b2529491c66f64c6f20dd
guilherme <guilherme.md@gmail.com>
2023-04-12 15:52:37 -0300

    feat: refactorin userDetails - oficial way

--- src/app/userDetails/[user]/page.tsx
+++ src/app/userDetails/[user]/page.tsx
@@ -9,33 +9,48 @@ export default async function UserDetails({
   const userName = params.user;
 
   const { userDetails } = await useUserDetails({ userName });
-  const { name, picture, email, phone, location, dob } = userDetails;
+  const { name, picture, email, phone, cell, location, dob, gender } =
+    userDetails;
   const formattedDate = format(new Date(dob.date), "dd/MM/yyyy");
-
+  console.log("street", location.street);
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 md:p-6 lg:p-8 flex flex-col md:flex-row">
-      <div className="flex-shrink-0">
-        <img
-          className="w-24 h-24 md:w-32 md:h-32 rounded-full object-cover"
-          src={picture.large}
-          alt={`${name.first} ${name.last}`}
-        />
-      </div>
-      <div className="md:ml-6 mt-4 md:mt-0">
-        <h2 className="text-xl font-bold mb-2">
-          {name.first} {name.last}
-        </h2>
-        <p className="text-gray-600 mb-4">{email}</p>
-        <p className="text-gray-600 mb-4">{phone}</p>
-        <div className="bg-gray-200 p-4 rounded-md">
-          <p className="text-lg font-medium mb-2">Data de nascimento:</p>
-          <p className="text-gray-700">{formattedDate}</p>
-          <p className="text-gray-700">{dob.age} anos</p>
-        </div>
-
+    <div className="flex flex-col items-center p-8">
+      <img
+        src={picture.large}
+        alt="User profile"
+        className="rounded-full w-40 h-40 mb-4"
+      />
+      <h2 className="text-2xl font-bold mb-4">
+        {name.title} {name.first} {name.last}
+      </h2>
+      <p className="text-gray-600 mb-4">{email}</p>
+      <div className="grid grid-cols-2 gap-4">
         <p className="text-gray-600">
-          {location.street.number} {location.street.name}, {location.city},{" "}
-          {location.state} {location.postcode}, {location.country}
+          <span className="font-bold">Gender:</span> {gender}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">DOB:</span> {formattedDate}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">Phone:</span> {phone}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">Cell:</span> {cell}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">Address:</span>
+          {location.street.name}, {location.street.number}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">State:</span>
+          {userDetails.location.state} ${location.postcode}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">Post Code:</span>
+          {location.postcode}
+        </p>
+        <p className="text-gray-600">
+          <span className="font-bold">Nationality:</span> {userDetails.nat}
         </p>
       </div>
     </div>
