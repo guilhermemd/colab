7b8338e797dc4e4298fd2dd339d98b0702230ae9
guilherme <guilherme.md@gmail.com>
2023-04-11 21:51:59 -0300

    feat: max-width on layout and kick off on userDetails

--- src/app/userDetails/[user]/page.tsx
+++ src/app/userDetails/[user]/page.tsx
@@ -7,6 +7,28 @@ export default async function UserDetails({
   const userName = params.user;
 
   const { userDetails } = await useUserDetails({ userName });
-  console.log("user details", userDetails);
-  return <div>User Details</div>;
+  const { name, picture, email, phone, location } = userDetails;
+
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 md:p-6 lg:p-8 flex flex-col md:flex-row">
+      <div className="flex-shrink-0">
+        <img
+          className="w-24 h-24 md:w-32 md:h-32 rounded-full object-cover"
+          src={picture.large}
+          alt={`${name.first} ${name.last}`}
+        />
+      </div>
+      <div className="md:ml-6 mt-4 md:mt-0">
+        <h2 className="text-xl font-bold mb-2">
+          {name.first} {name.last}
+        </h2>
+        <p className="text-gray-600 mb-4">{email}</p>
+        <p className="text-gray-600 mb-4">{phone}</p>
+        <p className="text-gray-600">
+          {location.street.number} {location.street.name}, {location.city},{" "}
+          {location.state} {location.postcode}, {location.country}
+        </p>
+      </div>
+    </div>
+  );
 }
